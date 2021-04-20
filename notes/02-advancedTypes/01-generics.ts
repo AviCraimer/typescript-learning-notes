12ef4e7ea1e23ca661d96cbdb24101162d91131a
AVI CRAIMER <AVI.CRAIMER@cbc.ca>
2021-04-20 16:03:13 -0400

    Generics and generic constraints notes

---
+++ notes/02-advancedTypes/01-generics.ts
@@ -0,0 +1,158 @@
+//Generic Types
+
+//We want to define a tree data structure, to store hierarchical data.
+
+//We can start by defining some types.
+
+//A tree node has a parent which is itself a tree node, or null in the case of the root of the tree.
+
+//A data property to store any arbitrary data
+
+//It has an array of children which contains a combination of further tree nodes
+type TreeNode = {
+    parent: TreeNode | null;
+    children: TreeNode[];
+    data: any;
+};
+
+//A tree root is a TreeNode with parent === null
+type TreeRoot = TreeNode & { parent: null };
+
+const tree: TreeRoot = {
+    parent: null,
+    data: 1,
+    children: [],
+};
+
+//Add some children
+tree.children = [
+    {
+        parent: tree,
+        data: 2,
+        children: [],
+    },
+    {
+        parent: tree,
+        data: 3,
+        children: [],
+    },
+    {
+        parent: tree,
+        data: "Randomly a string", // No compiler error when we mix data types inside the tree
+        children: [],
+    },
+];
+
+//Now I want to access a value in one of the nodes
+
+const data = tree.children[0].data;
+
+//Hover over it and you'll see that it has the any type. This isn't so useful since we want to know what type of data is stored in our tree.
+
+//Of course, we could create a specialized tree for every data type.
+
+//e.g.,
+type TreeNodeString = {
+    parent: TreeNodeString | null;
+    children: TreeNodeString[];
+    data: string;
+};
+
+type TreeNodeNumber = {
+    parent: TreeNodeNumber | null;
+    children: TreeNodeNumber[];
+    data: number;
+};
+
+//...etc for every tree type you might need. //This is quite tedious.
+// Notice how in the name of the type we put a type
+// TreeNode + string = TreeNodeString
+// What if we could actually pass string as a parameter? This is exactly what generic types allow you to do.
+
+//Generic types allow generality in the type definition together with specificity in the type of the instantiated value.
+
+//The T is a type parameter. i.e., it is a placeholder that can be filled in with any TypeScript type.
+type TreeNodeGeneric<T> = {
+    //We put type parameters between angle brackets, think of these as analogous to the smooth brackets of a regular function, i.e, <T> <=> (x)
+    parent: TreeNodeGeneric<T> | null;
+    children: TreeNodeGeneric<T>[];
+    data: T; // Notice that we put the type parameter here, where we previously had the any type.
+};
+
+type TreeRootGeneric<T> = TreeNodeGeneric<T> & { parent: null }; // For our tree root we can pass through the type parameter to our generic node
+
+//Now we copy our tree code from above but we assign the generic types
+//Notice that we pass in number as the type argument to fill in our T parameter
+//This means that everywhere we had T in our type definition, this type will now have number
+const treeGeneric: TreeRootGeneric<number> = {
+    parent: null,
+    data: 1,
+    children: [],
+};
+
+//Add some children
+treeGeneric.children = [
+    {
+        parent: tree,
+        data: 2,
+        children: [],
+    },
+    {
+        parent: tree,
+        data: 3,
+        children: [],
+    },
+    {
+        parent: tree,
+        data: "Randomly a string", // We get a helpful compiler error if we mix data types
+        children: [],
+    },
+];
+
+//Now when we retrieve our data, TypeScript correctly infers that we have a value of type number
+const numberData = treeGeneric.children[0].data;
+
+
+//Somewhat more advanced
+// We can define a custom tree mapping function for analogous to Array.prototype.map()
+
+const mapTree = <T, S>(
+    fn: (data: T) => S // Takes a callback function argument that is applied to the node data.
+): ((startTree: TreeRootGeneric<T>) => TreeRootGeneric<S>) => {
+
+    function mapTreeSteps (currentTree: TreeRootGeneric<T>, parent: null) : TreeRootGeneric<S>
+    function mapTreeSteps (currentTree: TreeNodeGeneric<T>, parent: TreeNodeGeneric<S>) : TreeNodeGeneric<S>
+    function mapTreeSteps (
+        currentTree: TreeNodeGeneric<T>,
+        parent: TreeNodeGeneric<S> | null
+    ): TreeNodeGeneric<S>  {
+
+        //Create a new node with mapped data
+        const newNode : TreeNodeGeneric<S> =  {
+            parent,
+            data: fn(currentTree.data), //Apply the provided call back function to the data
+            children: []
+        };
+
+        //Recursively apply the mapping to all children, passing in the newly created node as parent
+        newNode.children = currentTree.children.map(childNode => mapTreeSteps(childNode, newNode) )
+
+        return newNode;
+    };
+
+    //Return a function that applies the mapping for any given tree of type T
+    return (rootTree: TreeRootGeneric<T>):TreeRootGeneric<S> => mapTreeSteps(rootTree, null)
+};
+
+
+const hiHiHi = (num: number):string => {
+    const nat = Math.abs(Math.round(num))
+    const hiArr : "hi"[] = []
+    hiArr.length = nat;
+    hiArr.fill("hi")
+    return hiArr.join(" ")
+}
+
+const treeHi = mapTree(hiHiHi)(treeGeneric)
+
+const hiString = treeHi.children[1].data  // "hi hi hi"
---
+++ notes/02-advancedTypes/02- generic constraints.ts
@@ -0,0 +1,62 @@
+//Generic Constraints
+
+//Sometimes we want to allow a range of types but not literally every possible type.
+
+//Here is  function that takes in an object and uses the dataOfBirth field to calculate the person's age in years, adding this to the other fields and returning a new object.
+const _addAge = <T>(obj: T) => {
+    const milliseconds = Date.now() - obj.dateOfBirth.getTime(); //Compiler error because dateOfBirth might not be available on T
+    const years = Math.floor(milliseconds / 1000 / 60 / 60 / 24 / 365);
+
+    return {
+        ...obj,
+        age: years,
+    };
+};
+
+//The problem here is that T is too broad. It could be replaced by any type in type script, many of which do not have the dataOfBirth field.
+
+//We solve this problem with a generic constraint.
+
+const addAge = <T extends { dateOfBirth: Date }>(obj: T) => {
+    const milliseconds = Date.now() - obj.dateOfBirth.getTime();
+    const years = Math.floor(milliseconds / 1000 / 60 / 60 / 24 / 365.25);
+
+    return {
+        ...obj,
+        age: years,
+    };
+};
+
+const withAge = addAge({
+    name: "Lex Luthor",
+    dateOfBirth: new Date(1978, 5, 25),
+    email: "lex@gmail.com"
+});
+
+const age = withAge.age //Infers that age is type number
+
+const email = withAge.email // The typing preserves arbitrary extra fields based on generic inference
+
+
+// With literal types.
+
+// You can also use generic constraints to add literal types by extending string or number
+
+const setType = <T extends string, S extends object>(typeString: T, obj: S) => {
+    return {
+        ...obj,
+        type: typeString
+    }
+}
+
+const PERSON_TYPE_CONSTANT = "PERSON_TYPE_CONSTANT" // This has a literal type because it was declared with const
+
+const jill = setType(PERSON_TYPE_CONSTANT,  {
+    name: "Jill",
+    age: 25
+})
+
+//Check the type of jill. She has the properties that were passed in plus the additional type property with the literal type "PERSON_TYPE_CONSTANT". Without the use of a generic here, the type of the "type" property would be string.
+
+
+
