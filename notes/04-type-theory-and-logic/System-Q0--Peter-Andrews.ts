0ac4e92867ff6835f23524ce93e2295eb5f6ded9
Avi Craimer <mailavi@gmail.com>
2021-10-17 16:49:39 -0400

    Added truth, falsity, forall, conjunction.

--- notes/04-type-theory-and-logic/System-Q0--Peter-Andrews.ts
+++ notes/04-type-theory-and-logic/System-Q0--Peter-Andrews.ts
@@ -1,8 +1,9 @@
+// Page references are to:
 //An Introduction to Mathematical Logic and Type Theory: To Truth Through Proof
 // By Peter B. Andrews
 
 //System Q_0 is a version of higher-order logic based on simply typed lambda calculus with equality.
-// A cool feature of it is that it has only one rule of inference which is the substitution of equal terms for each other (a.k.a, Leibnitz's law. )
+// A cool feature of it is that it has only one rule of inference which is the substitution of equal terms for each other (a.k.a, Leibnitz's law.). The Q in the name of the system is related to Q in eQuals.
 
 //Page 210, section 51
 //(a) - (c)
@@ -82,7 +83,7 @@ const Q = (() => {
 
         const QInstance = QLookup.get(alphaStr) ?? {
             syntaxRole: ConstantBrand,
-            name: `[EQ_${alphaStr}]`,
+            name: `[Q_${alphaStr}]`,
             type: [alpha, [alpha, Om]],
             components: [],
         };
@@ -116,12 +117,24 @@ type FnApp = WFFBase & {
     components: [WFF, WFF];
 };
 
-const Apply = (a: WFF, b: WFF): FnApp | undefined => {
+const typeEq = (a: TypeExpression, b: TypeExpression): Boolean => {
+    if (typeof a === "symbol" && typeof b === "symbol") {
+        return a === b;
+    } else if (Array.isArray(a) && Array.isArray(b)) {
+        return typeEq(a[0], b[0]) && typeEq(a[1], b[1]);
+    } else {
+        return false;
+    }
+};
+
+const Apply = (a: WFF, b: WFF): FnApp => {
+    if (!a || !b) {
+        console.log("One of the types sent to Apply is not defined");
+        throw new Error();
+    }
+
     //Check if argument type of A matches type of B
-    if (
-        Array.isArray(a.type) &&
-        typeToString(a.type[0]) === typeToString(b.type)
-    ) {
+    if (Array.isArray(a.type) && typeEq(a.type[0], b.type)) {
         return {
             syntaxRole: FnAppBrand,
             type: a.type[1],
@@ -129,7 +142,14 @@ const Apply = (a: WFF, b: WFF): FnApp | undefined => {
             name: `[${a.name}(${b.name})]`,
         };
     } else {
-        return undefined;
+        console.log("Types sent to Apply do not match is not defined");
+        console.log(
+            `Type of first argument is
+            ${typeToString(a.type)}.
+            Second argument
+            ${typeToString(b.type)}`
+        );
+        throw new Error();
     }
 };
 
@@ -142,7 +162,7 @@ type LambdaTerm = WFFBase & {
     components: [WFF, WFF];
 };
 
-const Abstraction = (x: VariableSymbol, b: WFF): LambdaTerm | undefined => {
+const Abstract = (x: VariableSymbol, b: WFF): LambdaTerm => {
     return {
         syntaxRole: LambdaBrand,
         type: [x.type, b.type],
@@ -152,3 +172,48 @@ const Abstraction = (x: VariableSymbol, b: WFF): LambdaTerm | undefined => {
 };
 
 type WFF = PrimitiveTerm | FnApp | LambdaTerm;
+
+//p 212, Definitions
+const Q_Om = Q(Om);
+const BiBooleanPropEquality = Q(Q_Om.type);
+
+const T = Apply(Apply(BiBooleanPropEquality, Q(Om)), Q(Om)) as FnApp;
+
+//A function that takes any truth-value and always returns T
+const x_Om = getVariable(Om);
+const alwaysTrue = Abstract(x_Om, T);
+const id_Om = Abstract(x_Om, x_Om);
+
+//Now false is the equality of AlwaysTrue and Id_Om, since clearly this is false.
+const F = Apply(Apply(Q(alwaysTrue.type), alwaysTrue), id_Om);
+
+//Pi returns a ForAll constructor for each type. The ForAll constructor needs an additional argument of type alpha -> Om. This is some property of elements of alpha, and the completed ForAll proposition will be true iff the property equals a property that maps all alphas to True. In other words, only if the property is true of all alphas.
+const Pi = (alpha: TypeExpression) => {
+    const alphaStr = typeToString(alpha);
+
+    const Q_alphaPredicate = Q([alpha, Om]);
+
+    const allTrueAlpha = Abstract(getVariable(alpha), T);
+
+    return Apply(Q_alphaPredicate, allTrueAlpha);
+};
+
+// console.log(Pi(Ind));
+
+//Conjunction
+const and = () => {
+    const g = getVariable([Om, [Om, Om]]);
+    const x = getVariable(Om);
+    const y = getVariable(Om);
+    const gxy = Apply(Apply(g, x), y);
+    const leftSide = Abstract(g, gxy); // λg[g(x)(y)]
+
+    const gTT = Apply(Apply(g, T), T);
+    const rightSide = Abstract(g, gTT);
+
+    const andEquation = Apply(Apply(Q(leftSide.type), leftSide), rightSide);
+
+    const conjunction = Abstract(x, Abstract(y, andEquation));
+    console.log(conjunction.name);
+};
+and();
